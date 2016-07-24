05d6e46c83a6588c43051e73213e68e4ad8b1984
anurag145 <anurag145@gmail.com>
2016-07-24 21:49:21 +0530

    initial commit

--- app/src/main/java/com/github/anurag145/getlastaddress/MainActivity.java
+++ app/src/main/java/com/github/anurag145/getlastaddress/MainActivity.java
@@ -5,15 +5,34 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    NetworkInfo activeNetwork;
+     private TextView textView;
+    private Button button;
+     private  NetworkInfo activeNetwork;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView=(TextView)findViewById(R.id.textView);
+        button=(Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(networkstate(getApplicationContext()))
+                {
+
+                }
+            }
+        });
+
+
     }
 
     boolean networkstate(Context context)
