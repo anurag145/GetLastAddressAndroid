d8523c5dc5557436699d5b120f9d90f1c35e5b51
anurag145 <anurag145@gmail.com>
2016-08-01 10:03:01 +0530

    permissions

--- app/src/main/java/com/github/anurag145/getlastaddress/MainActivity.java
+++ app/src/main/java/com/github/anurag145/getlastaddress/MainActivity.java
@@ -1,8 +1,11 @@
 package com.github.anurag145.getlastaddress;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,10 +17,33 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
      private  NetworkInfo activeNetwork;
 
+    private int PERMISSION_CODE_1 = 23;
+    public static final String PACKAGE_NAME =
+            "com.github.anurag145.getlastaddress";
+    public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
+    public static final String RESULT_DATA_KEY = PACKAGE_NAME + ".RESULT_DATA_KEY";
+
+    public static final String LOCATION_LATITUDE_DATA_EXTRA = PACKAGE_NAME + ".LOCATION_LATITUDE_DATA_EXTRA";
+    public static final String LOCATION_LONGITUDE_DATA_EXTRA = PACKAGE_NAME + ".LOCATION_LONGITUDE_DATA_EXTRA";
+
+    public static final int SUCCESS_RESULT = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= 23) {
+
+            if ( ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+                }
+
+                requestpermisions();
+
+
+            }
+        }
 
         textView=(TextView)findViewById(R.id.textView);
         button=(Button)findViewById(R.id.button);
@@ -33,6 +59,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+    public void requestpermisions() {
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_CODE_1);
     }
 
     boolean networkstate(Context context)
